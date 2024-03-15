6a48f34acd14937e9eb2894f3f289222a02f3f71
IamSwis <dre12sims@gmail.com>
2024-03-15 17:58:16 -0400

    P1

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <h6>Lobby Tech</h6>
+      <h4>By Will</h4>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+/**
+ * Components:
+ * are functions
+ * start with capital letters
+ * should always return something
+ *
+ * are usually exported
+ */
+
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div>
+      <h1>Navbar will be here</h1>
+      <button>Test 1</button>
+      <button>Test 2</button>
+      <button>Test 3</button>
+    </div>
+  );
+}
+
+export default Navbar;
